84cd4656780e6004f4c70daecbab2215e9e1b146
nimaomidsajedi <nimaomidsajedi@users.noreply.github.com>
2021-01-30 11:57:38 +0100

    Initial commit

---
+++ Tkinter_HelloWorld/main.py
@@ -0,0 +1,14 @@
+# Created by: Nima Omidsajedi
+
+from tkinter import *
+
+root = Tk() # Must be used in every Kinter project
+
+myLabel = Label(root, text="Hello World!")  # Create a Label widget from Root
+
+# Pack the Label you want to show in the screen
+myLabel.pack()
+
+# Loop the project until you end it
+root.mainloop()
+
